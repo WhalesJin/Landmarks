a8b8c01804bc6cd96fffaab1e45397cc1de4f5eb
Whales <Whales.w.Jin@gmail.com>
2023-10-10 04:36:13 +0900

    ✨ Feat: ModelData 객체를 관찰하도록 Environment 키워드 활용

--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -30,6 +30,8 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
+        let landmarks = ModelData().landmarks
+        
         VStack {
             LandmarkRow(landmark: landmarks[0])
             LandmarkRow(landmark: landmarks[1])
